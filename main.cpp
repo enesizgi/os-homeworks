daa6795bfd4a779a97b7796d2e2afd53b8390efa
Enes izgi <45923323+enesizgi@users.noreply.github.com>
2022-06-20 21:29:35 +0300

    initiate ls_command function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <string>
+#include "fat32.h"
+#include "parser.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+void ls_command (parsed_input& input, FILE& imgFile, BPB_struct& BPBstruct) {
+
+}
+
+int main(int argc, char *argv[]) {
+    bool is_development = true; // TODO: Change this to false before submission
+    if (argc != 2) {
+        exit(1);
+    }
+
+    string filename("/home/enes/os-hw3/");
+    filename.append(argv[1]);
+
+    FILE* imgFile;
+    imgFile = fopen(is_development ? filename.c_str() : argv[1],"r+");
+    if (imgFile == nullptr) {
+        fclose(imgFile);
+        exit(1);
+    }
+
+    BPB_struct* BPBstruct = new BPB_struct;
+    size_t result = fread(BPBstruct, sizeof(BPB_struct), 1, imgFile);
+    if (result != 1) {
+        exit(1);
+    }
+
+    while (true) {
+        string line;
+        std::getline(std::cin, line);
+        parsed_input* input = new parsed_input;
+        char* input_chr = new char[line.length() + 1];
+        strcpy(input_chr, line.c_str());
+        parse(input, input_chr);
+
+        if (input->type == LS) {
+            ls_command(*input, *imgFile, *BPBstruct);
+        }
+    }
     return 0;
 }
