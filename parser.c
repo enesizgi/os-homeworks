d52d73c6ef1aae128cae873df92dbc21f4df0a3d
Enes İzgi <enesizgi@tutanota.com>
2022-06-24 16:33:25 +0300

    update pdf and parser.c

--- parser.c
+++ parser.c
@@ -3,6 +3,7 @@
 void parse(parsed_input* inp, char *line) {
     char *tmp;
     unsigned long size;
+
     size = strlen(line);
 
     if ( line[size-1] == '\n' )
@@ -34,7 +35,7 @@ void parse(parsed_input* inp, char *line) {
 
     size = strlen(tmp);
 
-    inp->arg1 = (char*) calloc(size, sizeof(char));
+    inp->arg1 = (char*) calloc(size+1, sizeof(char));
     strcpy(inp->arg1, tmp);
 
     tmp = strtok(NULL, " ");
@@ -42,7 +43,7 @@ void parse(parsed_input* inp, char *line) {
     if ( tmp ) {
         size = strlen(tmp);
 
-        inp->arg2 = (char*) calloc(size, sizeof(char));
+        inp->arg2 = (char*) calloc(size+1, sizeof(char));
         strcpy(inp->arg2, tmp);
     }
 }
