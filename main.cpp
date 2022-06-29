d92d77d1fe7d74ed4b3c551aeac55d4ee0eb4f31
Enes izgi <45923323+enesizgi@users.noreply.github.com>
2022-06-30 00:38:57 +0300

    add cd code and utility functions

--- main.cpp
+++ main.cpp
@@ -6,8 +6,112 @@
 
 using namespace std;
 
-string resolve_path (parsed_input& input, string& current_working_dir) {
-    return "";
+uint32_t current_dir_cluster_no;
+
+uint32_t get_fat_entry_at (FILE*& imgFile, uint32_t location);
+vector<uint32_t>* find_entries (uint32_t& first_cluster, FILE*& imgFile, BPB_struct& BPBstruct, uint16_t& beginning_of_fat_table, uint32_t& end_of_chain);
+string lfn_name_extract (FatFileEntry*& file_entry);
+
+vector<FatFileEntry> get_fatFileEntries_in_cluster(uint32_t cluster_no, FILE*& imgFile, BPB_struct& BPBstruct) {
+    auto bytes_per_cluster = BPBstruct.BytesPerSector*BPBstruct.SectorsPerCluster;
+    auto beginning_of_clusters = (BPBstruct.ReservedSectorCount+BPBstruct.extended.FATSize*BPBstruct.NumFATs)*BPBstruct.BytesPerSector;
+
+    fseek(imgFile, beginning_of_clusters + (cluster_no - BPBstruct.extended.RootCluster) * bytes_per_cluster, SEEK_SET);
+    int size_of_fatFileEntry = sizeof(FatFileEntry);
+    vector<FatFileEntry> FatFileEntries;
+    for (int j = 0; j < bytes_per_cluster; j += size_of_fatFileEntry) {
+        auto *tmp_file = new FatFileEntry;
+        fread(tmp_file, size_of_fatFileEntry, 1, imgFile);
+        FatFileEntries.push_back(*tmp_file);
+        delete tmp_file;
+    }
+    return FatFileEntries;
+}
+
+bool is_path_valid (vector<string>& path, FILE*& imgFile, BPB_struct& BPBstruct) {
+    if (path.empty()) return false;
+    uint16_t beginning_of_fat_table = BPBstruct.ReservedSectorCount*BPBstruct.BytesPerSector;
+    uint32_t end_of_chain = get_fat_entry_at(imgFile, beginning_of_fat_table);
+
+    uint32_t nextCluster = BPBstruct.extended.RootCluster;
+    for (int i = 0; i < path.size() - 1; i++) {
+        auto* root_fat_entries_p = find_entries(nextCluster, imgFile, BPBstruct, beginning_of_fat_table, end_of_chain);
+        auto& root_fat_entries = *root_fat_entries_p;
+        bool is_folder_found = false;
+        string FatFileEntryName;
+        for (int j = 0; j < root_fat_entries.size(); j++) {
+            auto FatFileEntries = get_fatFileEntries_in_cluster(root_fat_entries[j], imgFile, BPBstruct);
+            for (auto & FatFileEntrie : FatFileEntries) {
+                if (FatFileEntrie.lfn.attributes == 15) {
+                    FatFileEntry* tmp = &FatFileEntrie;
+                    FatFileEntryName.insert(0, lfn_name_extract(tmp));
+                }
+                else {
+                    if ((FatFileEntrie.msdos.attributes & 0x10) == 0x10 && path[i+1] == FatFileEntryName) {
+                        is_folder_found = true;
+                        nextCluster = FatFileEntrie.msdos.firstCluster;
+                    }
+                    FatFileEntryName.clear();
+                }
+            }
+            if (is_folder_found) break;
+        }
+        delete root_fat_entries_p;
+        if (!is_folder_found) return false;
+    }
+//    cout << path[0] << endl;
+    current_dir_cluster_no = nextCluster;
+    return true;
+}
+
+string resolve_path (string& path, string& current_working_dir, FILE*& imgFile, BPB_struct& BPBstruct) {
+    string s = path.starts_with('/') ? path + "/" : current_working_dir + (current_working_dir != "/" ? "/" : "") + path + "/";
+    s = path == "/" ? path : s;
+    std::string delimiter = "/";
+
+    size_t pos = 0;
+    std::string token;
+    vector<string> result;
+    while ((pos = s.find(delimiter)) != std::string::npos) {
+        token = s.substr(0, pos);
+        result.push_back(token);
+        s.erase(0, pos + delimiter.length());
+    }
+    vector<string> clean_result;
+    for (auto & i : result) {
+        if (i.empty()) {
+            clean_result.push_back(i);
+        }
+        else if (i == ".") {
+            continue;
+        }
+        else if (i == "..") {
+            // TODO: MAYBE CHECK HERE IF CLEAN_RESULT IS EMPTY (IT SHOULDN'T BE EMPTY EVER)
+            string tmp = clean_result.back();
+            if (tmp.empty()) {
+                continue;
+            }
+            clean_result.pop_back();
+        }
+        else clean_result.push_back(i);
+    }
+    if (result.empty()) {
+        return current_working_dir;
+    }
+
+    if (!is_path_valid(clean_result, imgFile, BPBstruct)) {
+        return current_working_dir;
+    }
+
+    s.clear();
+    for (int i = 0; i < clean_result.size(); i++) {
+        if (i == clean_result.size() - 1 && clean_result.size() != 1) {
+            s += clean_result[i];
+        }
+        else s += clean_result[i] + "/";
+    }
+
+    return s;
 }
 
 string lfn_name_extract (FatFileEntry*& file_entry) {
@@ -48,8 +152,8 @@ vector<uint32_t>* find_entries (uint32_t& first_cluster, FILE*& imgFile, BPB_str
     fseek(imgFile, beginning_of_fat_table+8+(first_cluster - BPBstruct.extended.RootCluster)*4, SEEK_SET);
     auto* fat_entries_p = new vector<uint32_t>;
     auto& fat_entries = *fat_entries_p;
-    fat_entries.push_back(BPBstruct.extended.RootCluster);
-    auto* tmp_entry = new uint32_t;
+    fat_entries.push_back(first_cluster);
+    auto* tmp_entry = new uint32_t; // TODO: NEED TO TEST WITH CHAIN FOLDERS. THIS IS PROBABLY WRONG BECAUSE OF LITTLE ENDIAN. SHOULD USE GET_FAT_ENTRY
     while (true) {
         fread(tmp_entry, sizeof(uint32_t), 1, imgFile);
         if (*tmp_entry == end_of_chain) break;
@@ -62,22 +166,32 @@ vector<uint32_t>* find_entries (uint32_t& first_cluster, FILE*& imgFile, BPB_str
     return fat_entries_p;
 }
 
+void cd_command (parsed_input& input, FILE*& imgFile, BPB_struct& BPBstruct, string& current_working_dir) {
+    // TODO: Handle deleted directories
+    if (input.arg1 == nullptr) {
+        return;
+    }
+    string path(input.arg1);
+    current_working_dir = resolve_path(path, current_working_dir, imgFile, BPBstruct);
+}
+
 void ls_command (parsed_input& input, FILE*& imgFile, BPB_struct& BPBstruct, string& current_working_dir) {
+    // TODO: Handle deleted directories and files.
     string arg1 = input.arg1 != nullptr ? input.arg1 : "";
     string arg2 = input.arg2 != nullptr ? input.arg2 : "";
+
     uint16_t beginning_of_fat_table = BPBstruct.ReservedSectorCount*BPBstruct.BytesPerSector;
     uint32_t end_of_chain = get_fat_entry_at(imgFile, beginning_of_fat_table);
-    auto* root_fat_entries_p = find_entries(BPBstruct.extended.RootCluster, imgFile, BPBstruct, beginning_of_fat_table, end_of_chain);
+    auto* root_fat_entries_p = find_entries(current_dir_cluster_no, imgFile, BPBstruct, beginning_of_fat_table, end_of_chain);
     auto& root_fat_entries = *root_fat_entries_p;
     auto bytes_per_cluster = BPBstruct.BytesPerSector*BPBstruct.SectorsPerCluster;
     auto beginning_of_clusters = (BPBstruct.ReservedSectorCount+BPBstruct.extended.FATSize*BPBstruct.NumFATs)*BPBstruct.BytesPerSector;
     int print_counter = 0;
-//    for (int i = 0; i < root_fat_entries.size(); i++) {
+    string str1; // TODO: MAYBE SHOULD MOVE THIS TO A LEVEL UP OUTSIDE THIS FOR LOOP. (DONE)
     for (auto& root_fat_entry : root_fat_entries) {
         fseek(imgFile, beginning_of_clusters + (root_fat_entry-2)*bytes_per_cluster, SEEK_SET);
         int size_of_fatFileEntry = sizeof(FatFileEntry);
         auto* tmp_file = new FatFileEntry;
-        string str1; // TODO: MAYBE SHOULD MOVE THIS TO A LEVEL UP OUTSIDE THIS FOR LOOP.
         for (int j = 0; j < bytes_per_cluster; j+=size_of_fatFileEntry) {
             fread(tmp_file, size_of_fatFileEntry, 1, imgFile);
             if (tmp_file->lfn.attributes == 15) { // It is LFN.
@@ -87,17 +201,17 @@ void ls_command (parsed_input& input, FILE*& imgFile, BPB_struct& BPBstruct, str
                 if (str1.length() > 0) {
                     if (arg1 == "-l") {
                         if ((tmp_file->msdos.attributes & 0x10) == 0x10) { // It is a directory.
+                            if (j != 0 && print_counter != 0) cout << endl;
                             cout << "drwx------ 1 root root 0 " << "<last_modified_date_and_time>" << " " << str1;
-                            if (j != bytes_per_cluster - size_of_fatFileEntry) cout << endl;
                         }
                         else {
+                            if (j != 0 && print_counter != 0) cout << endl;
                             cout << "-rwx------ 1 root root " << tmp_file->msdos.fileSize << " " << "<last_modified_date_and_time>" << " " << str1;
-                            if (j != bytes_per_cluster - size_of_fatFileEntry) cout << endl;
                         }
                     }
                     else {
+                        if (j != 0 && print_counter != 0) cout << " ";
                         cout << str1;
-                        if (j != bytes_per_cluster - size_of_fatFileEntry) cout << " ";
                     }
                     print_counter++;
                     str1.clear();
@@ -106,7 +220,7 @@ void ls_command (parsed_input& input, FILE*& imgFile, BPB_struct& BPBstruct, str
         }
         delete tmp_file;
     }
-    if (print_counter > 0 && arg1 != "-l") cout << endl;
+    if (print_counter > 0) cout << endl;
     root_fat_entries.clear();
     delete root_fat_entries_p;
 }
@@ -134,6 +248,8 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    current_dir_cluster_no = BPBstruct->extended.RootCluster;
+
     while (true) {
         cout << current_working_dir << "> ";
         string line;
@@ -154,8 +270,7 @@ int main(int argc, char *argv[]) {
             ls_command(*input, imgFile, *BPBstruct, current_working_dir);
         }
         else if (input->type == CD) {
-            current_working_dir = resolve_path(*input, current_working_dir);
-//            cd_command(*input, imgFile, *BPBstruct, current_working_dir);
+            cd_command(*input, imgFile, *BPBstruct, current_working_dir);
         }
 
         clean_input(input);
